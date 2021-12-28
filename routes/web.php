628e1634352c7924ed7540242aeb69f7de2472d6
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-12-28 10:45:43 +0100

    Create Todo Model, Create todolist database

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\TodosController@index');
+
+Route::resource('todos','App\Http\Controllers\TodosController');
+
