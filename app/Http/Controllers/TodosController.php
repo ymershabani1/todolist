0f004ea3f6afb05f44ecf0b3f9b76dec0ca7a32c
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-12-28 15:35:34 +0100

    Create index view, Create layout app.blade.php, Create custom scss file

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodosController extends Controller
@@ -13,7 +14,9 @@ class TodosController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::get();
+
+        return view('index')->with('todos', $todos);
     }
 
     /**
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Todos</h1>
+
+    @if(count($todos) > 0)
+        @foreach($todos as $todo)
+            <div class="card m-2">
+                <h2>{{ $todo->title }}</h2>
+                <span class="label label-danger">{{$todo->due}}</span>
+            </div>
+        @endforeach
+    @endif
+@endsection
