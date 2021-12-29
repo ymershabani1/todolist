4b866ebb26320219e7874f8d31555296cd23b750
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-12-29 19:31:19 +0100

    Save data to DB,  Create create blade php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <form>
+    <h1>Create a new Todo</h1>
+    <form method="post" action="/todo">
+        @csrf
         <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="title">Title</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="Enter title" value="{{ old('title') }}">
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <label for="content">Content</label>
+            <input type="text" class="form-control" name="content" id="content" placeholder="Enter content" value="{{ old('content') }}">
         </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="form-group">
+            <label for="due">Due</label>
+            <input type="text" class="form-control" name="due" id="due" placeholder="Enter due" value="{{ old('due') }}">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </form>
 @endsection
